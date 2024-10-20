b1c3d63d3f2586eaa34b6e253d1469c9c4dc81a2
Toru Nagai <nagato0614@gmail.com>
2024-10-20 12:30:11 +0900

    ✨ 新機能(enum_tutorial, searchstring): Rustプロジェクトに新しい列挙型とモジュールを追加
    - `enum_tutorial`に`IpAddr`と`Message`列挙型を追加し、`Message`に`call`メソッドを実装
    - 新しいRustプロジェクト`searchstring`を作成し、基本的な`main.rs`を追加

--- rust/enum_tutorial/src/main.rs
+++ rust/enum_tutorial/src/main.rs
@@ -1,3 +1,36 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message
+{
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
+enum Coin
+{
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 }
